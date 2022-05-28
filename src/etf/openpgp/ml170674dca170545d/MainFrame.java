68aa8bd5f8d5438fc83cff9662a2962b81d8ed39
LazarMihailovic <lazarcarcar@gmail.com>
2022-05-28 21:56:31 +0200

    Creation of menubar and new key gen menu item

--- src/etf/openpgp/ml170674dca170545d/MainFrame.java
+++ src/etf/openpgp/ml170674dca170545d/MainFrame.java
@@ -1,20 +1,41 @@
 package etf.openpgp.ml170674dca170545d;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 
 public class MainFrame extends JFrame{
 	private JFrame frame;
+	
 	
 	public MainFrame() {
 		super("OpenPGP");
 		
 		setupMainFrame();
+		setupMenuBar();
+		
+		frame.setVisible(true);
 	}
 	
 	private void setupMainFrame() {
 		frame = this;
-		frame.setBounds(450, 200, 600, 450);
+        frame.setSize(600, 600);
+        frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		frame.setLayout(new BorderLayout());
+	}
+	
+	private void setupMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem generatedNewKeysItem = new JMenuItem("Generate new key pair");
+
+        menu.add(generatedNewKeysItem);
+        menuBar.add(menu);
+        frame.add(menuBar, BorderLayout.NORTH);
 	}
 }
